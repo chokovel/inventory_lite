26c9eec912705b921197d7f4eb1853cfe7854a7e
theRabbi <tipson664@gmail.com>
2023-05-15 15:36:07 +0100

    minor change

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -50,56 +50,7 @@
                         </div>
                     </div>
                 @endforeach
-
-
-                {{-- <div class="col-md-6 col-lg-3">
-                    <div class="card">
-                        <img src="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/L/N/121271_1611814481.jpg"
-                            class="card-img-top" alt="Product 2">
-                        <div class="card-body">
-                            <h5 class="card-title">Product 2</h5>
-                            <p class="card-text">Colors: Green, Yellow</p>
-                            <p class="card-text">Sizes: Large, XL</p>
-                            <p class="card-text">Qty: 100</p>
-                            <a href="#" class="btn btn-primary">Add to Cart</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-6 col-lg-3">
-                    <div class="card">
-                        <img src="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/L/N/121271_1611814481.jpg"
-                            class="card-img-top" alt="Product 3">
-                        <div class="card-body">
-                            <h5 class="card-title">Product 3</h5>
-                            <p class="card-text">Colors: Black, White</p>
-                            <p class="card-text">Sizes: Small, Medium, Large</p>
-                            <p class="card-text">Qty: 100</p>
-                            {{-- <input type="number" class="form-control" min="1" value="1"> --}}
-                {{-- <a href="#" class="btn btn-primary">Add to Cart</a>
             </div>
         </div>
-    </div> --}}
-
-                {{-- <div class="col-md-6 col-lg-3">
-                    <div class="card">
-                        <img src="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/L/N/121271_1611814481.jpg"
-                            class="card-img-top" alt="Product 4">
-                        <div class="card-body">
-                            <h5 class="card-title">Product 4</h5>
-                            <p class="card-text">Colors: Pink, Purple</p>
-                            <p class="card-text">Sizes: Medium, Large</p>
-                            <p class="card-text">Qty: 100</p>
-
-                            <a href="#" class="btn btn-primary">delete</a>
-                        </div>
-                    </div>
-                </div> --}}
-
-            </div>
-        </div>
-
-
-        <!-- end main content section -->
     </div>
 @endsection
