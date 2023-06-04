5609387f2732962dfaa5b627d8952d1f699e4c27
theRabbi <tipson664@gmail.com>
2023-06-04 10:30:28 +0100

    styled login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,12 +14,11 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @livewireStyles
 </head>
 
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
-        @include('layouts.navigation')
+        {{-- @include('layouts.navigation') --}}
 
         <!-- Page Heading -->
         @if (isset($header))
